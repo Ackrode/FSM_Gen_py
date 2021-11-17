b2191a3a6c541027f71f35eaf55f1cd0c8afcf84
Ackrode <92236579+Ackrode@users.noreply.github.com>
2021-11-17 15:29:52 -0700

    aaaa

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
-from get_info import *
-
+from get_info import get_info
+from next_state import next_state
+from Output_Logic import output_logic
+from state import get_state
 table='state_table.txt'
-module, inputs, outputs, states = get_info(table)
-# get_info(table)
-print(module)
+path='design.v'
+module, inputs, outputs, states=get_info(table)
 print(inputs)
-print(outputs)
-print(states)
+get_state(path, module, inputs, outputs, states)
+next_state(path, inputs, states)
+print(inputs)
+output_logic(path,inputs, outputs, states)
+
+# get_info(table)
--- next_state.py
+++ next_state.py
@@ -1,17 +1,11 @@
-from get_info import get_info
 def get_n(inputs=[]):
     n=0
-
-
     if inputs:
 # Iterate through the inputs
         for i in inputs.get('size'):
             N1=int(i[1])
             N2=int(i[3])
-            print(N1,N2)
             n=n+abs(N1-N2)+1
- 
-
     return n
 def convert(s):
     # initialization of string to ""
@@ -24,37 +18,34 @@ def convert(s):
         else:
             new =new+ x +', '  
     return new
-with open('design.sv','w', encoding='utf-8') as f:    
-    design=['']
-    table='state_table.txt'
-    module, inputs, outputs, states=get_info(table)
-    #delete first 2 keys of inputs.get('name') dictionary
-    del inputs.get('name')[0]
-    del inputs.get('size')[0]
-    del inputs.get('name')[0]
-    del inputs.get('size')[0]
-    print(inputs)
-    n=get_n(inputs)
-    print(n)
-    design.append('always @(state ,'+convert(inputs.get('name'))+')')
-    design.append('\tbegin')
-    design.append('\t\tcase(state)')
-    i=0
-    for current_state in states.get('presentes_states'):
-        design.append('\t\t\t'+current_state+':')
-        design.append('\t\t\t\tbegin')
-        for j in range(0,n+1):
-            #if its the last element of the list
-            if j==n:
-                design.append ('\t\t\t\t\telse')
-                design.append('\t\t\t\t\t\tnext_state<='+states.get('futures_states')[i][j]+';')
-            else:
-                design.append ('\t\t\t\t\tif('+convert(inputs.get('name'))+'=='+str(j)+')')
-                design.append('\t\t\t\t\t\tnext_state<='+states.get('futures_states')[i][j]+';')
-        i+=1
-    design.append('\t\tendcase')
-    design.append('\tend')
-    #print(design)
-    for line in design:
-        f.write(line+'\n')
-    f.close()
+def next_state(path, inputs=None,states=None):
+    with open(path,'w', encoding='utf-8') as f:    
+        design=['']
+        #delete first 2 keys of inputs.get('name') dictionary
+        del inputs.get('name')[0]
+        del inputs.get('size')[0]
+        del inputs.get('name')[0]
+        del inputs.get('size')[0]
+        n=get_n(inputs)
+        design.append('always @(state ,'+convert(inputs.get('name'))+')')
+        design.append('\tbegin')
+        design.append('\t\tcase(state)')
+        i=0
+        for current_state in states.get('presentes_states'):
+            design.append('\t\t\t'+current_state+':')
+            design.append('\t\t\t\tbegin')
+            for j in range(0,n+1):
+                #if its the last element of the list
+                if j==n:
+                    design.append ('\t\t\t\t\telse')
+                    design.append('\t\t\t\t\t\tnext_state<='+states.get('futures_states')[i][j]+';')
+                else:
+                    design.append ('\t\t\t\t\tif('+convert(inputs.get('name'))+'=='+str(j)+')')
+                    design.append('\t\t\t\t\t\tnext_state<='+states.get('futures_states')[i][j]+';')
+            i+=1
+        design.append('\t\tendcase')
+        design.append('\tend')
+        #print(design)
+        for line in design:
+            f.write(line+'\n')
+        f.close()
