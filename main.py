f5a72c082fe89d7ce57568baeb845b82c437b0a4
Ackrode <92236579+Ackrode@users.noreply.github.com>
2021-11-17 15:52:45 -0700

    append and dictionary copy solution

--- main.py
+++ main.py
@@ -2,13 +2,14 @@ from get_info import get_info
 from next_state import next_state
 from Output_Logic import output_logic
 from state import get_state
+import copy
 table='state_table.txt'
 path='design.v'
 module, inputs, outputs, states=get_info(table)
-print(inputs)
+
+input=copy.deepcopy(inputs)
 get_state(path, module, inputs, outputs, states)
-next_state(path, inputs, states)
-print(inputs)
+next_state(path, input, states)
 output_logic(path,inputs, outputs, states)
 
 # get_info(table)
