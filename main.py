7c443935199aed9a5cd4c637a7bf3a092be365b4
Ackrode <92236579+Ackrode@users.noreply.github.com>
2021-11-22 00:21:56 -0700

    tb generated

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from get_info import get_info
 from next_state import next_state
 from Output_Logic import output_logic
-from state import get_state
+from State import get_state
+from backbone import backbone
+from stimulus import stimulus
 import copy
 table='state_table.txt'
 path='design.v'
@@ -11,4 +13,6 @@ get_state(path, module, inputs, outputs, states)
 next_state(path, input, states)
 output_logic(path,inputs, outputs, states)
 
+backbone(inputs, outputs,module=module.get('name'))
+stimulus(module.get('name'), states)
 # get_info(table)
